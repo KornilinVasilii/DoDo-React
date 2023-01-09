98e588e6ca4b3f177c6fe84f59c8220fcb2dacd4
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-09 17:16:11 +0300

    footer

--- client/src/UI/Footer/Footer.jsx
+++ client/src/UI/Footer/Footer.jsx
@@ -4,8 +4,7 @@ import { Container } from "../../components/Container/Container";
 import { FooterContacts } from "../../components/FooterContacts/FooterContacts";
 import { Pizzerias } from "../../components/Pizzerias/Pizzerias";
 import { Profit } from "../../components/Profit/Profit";
-import { Social } from "../../components/Social/Social";
-import { Work } from "../../components/Work/Work";
+
 import s from './Footer.module.css'
 
 export function Footer() { 
@@ -14,12 +13,10 @@ export function Footer() {
       <Container>
         <div className={s.wrapper}>
           <About />
-          <Work />
           <FooterContacts />
         </div>
         <div className={s.profit}>
-          <Profit
-          />
+          <Profit/>
           <Pizzerias/>
         </div>
         <Information /> 
--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,12 +1,43 @@
 import s from './About.module.css'
 export function About() {
   return (
-    <div className={s.about}>
-      <h3 className={s.title}>Додо Пицца</h3>
-      <a href='#' className={s.item}>О нас</a>
-      <a href='#' className={s.item}>Додо-книга</a>
-      <a href='#' className={s.item}>Блог «Сила ума» </a>
-      <a href='#' className={s.item}>Додо ИС</a>
-    </div>
+    <>
+      <div className={s.about}>
+        <h3 className={s.title}>Додо Пицца</h3>
+        <a href="#" className={s.item}>
+          О нас
+        </a>
+        <a href="#" className={s.item}>
+          Додо-книга
+        </a>
+        <a href="#" className={s.item}>
+          Блог «Сила ума»{" "}
+        </a>
+        <a href="#" className={s.item}>
+          Додо ИС
+        </a>
+      </div>
+      <div className={s.work}>
+        <h3 className={s.title}>Работа</h3>
+        <a href="#" className={s.item}>
+          В пицерии{" "}
+        </a>
+        <a href="#" className={s.item}>
+          В контакт-центре{" "}
+        </a>
+      </div>
+      <div className={s.partners}>
+        <h3 className={s.title}>Партнерам</h3>
+        <p className={s.item}>Франшиза</p>
+        <p className={s.item}>Инвестиции</p>
+        <p className={s.item}>Поставщикам</p>
+        <p className={s.item}>Предложить помещение</p>
+      </div>
+      <div className={s.interesting}>
+        <h3 className={s.title}>Это интересно</h3>
+        <p className={s.item}> Экскурсии и мастер-классы</p>
+        <p className={s.item}>Корпоративные заказы</p>
+      </div>
+    </>
   );
 }

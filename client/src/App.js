003b59d98652667d109872955688e7c3b567a48e
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-10 13:33:56 +0300

    delete basket from widgets

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import './index.css'
 import { Footer } from './UI/Footer/Footer';
 import { Header } from './UI/Header/Header';
 import { Main } from './UI/Main/Main'
-// import { Basket } from './pages/Basket/Basket';
+import { Basket } from './pages/Basket/Basket';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <Footer/>
+      <Footer />
+      <Basket/>
     </>
   );
 }

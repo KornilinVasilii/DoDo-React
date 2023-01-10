0b58ae25fd1f933b67fdc94deec622d647fea42f
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-10 13:23:25 +0300

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import './index.css'
 import { Footer } from './UI/Footer/Footer';
 import { Header } from './UI/Header/Header';
 import { Main } from './UI/Main/Main'
+// import { Basket } from './widgets/Basket/Basket'
+
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <Footer/>
+      <Footer />
+      {/* <Basket/> */}
     </>
   );
 }
--- client/src/widgets/Basket/Basket.jsx
+++ client/src/widgets/Basket/Basket.jsx
@@ -1,10 +1,14 @@
-import s from './Basket.module.css'
-import { Logo } from '.components/Logo/Logo';
+// import s from './Basket.module.css'
+// import { Logo } from '.components/Logo/Logo';
+
+
+import { Logo } from "../../components/Logo/Logo";
 
 export function Basket() {
   return (
     <>
-      <Logo/>
+      <Logo />
+    
     </>
   )
 }

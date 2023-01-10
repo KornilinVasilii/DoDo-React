8054b0b6b808083bba3e886fa9bfa5ad9b0fb568
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-10 19:31:25 +0300

    prod

--- client/src/pages/Basket/Basket.jsx
+++ client/src/pages/Basket/Basket.jsx
@@ -2,7 +2,7 @@ import s from '../Basket/Basket.module.css';
 import { Logo } from "../../components/Logo/Logo"
 import { BasketHeader } from '../../widgets/BasketHeader/BasketHeader';
 import { Container } from '../../components/Container/Container';
-import { BasketFooter } from '../BasketFooter/BasketFooter';
+import { BasketFooter } from '../../widgets/BasketFooter/BasketFooter';
 
 export function Basket() {
   return (
--- client/src/pages/Order/Order.jsx
+++ client/src/pages/Order/Order.jsx
@@ -2,33 +2,29 @@ import s from "./Order.module.css";
 import { BasketHeader } from "../../widgets/BasketHeader/BasketHeader";
 import { Container } from "../../components/Container/Container";
 import { Input } from "../../widgets/Input/Input";
+import { BasketFooter } from "../../widgets/BasketFooter/BasketFooter";
 
 export function Order() {
   return (
     <div>
       <BasketHeader />
       <Container>
+        <h1 className={s.order_title}>Заказ на доставку</h1>
         <div className={s.order}>
-          <h1 className={s.order_title}>Заказ на доставку</h1>
-          <div className={s.contacts}>
-            <div className={s.order_info}>
-              <p className={s.info}>Имя</p>
-              <Input />
-            </div>
-            <div className={s.order_info}>
-              <p className={s.info}>Номер телефона</p>
-              <Input />
-            </div>
-            <div className={s.order_info}>
-              <p className={s.info}>Куда доставить</p>
-              <input className={s.address} type="text-aria" />
-            </div>
+          <div className={s.order_info}>
+            <p className={s.info}>Имя</p>
+            <p className={s.info}>Номер телефона</p>
+            <p className={s.info}>Куда доставить</p>
           </div>
-          <>
-            <div className={s.details}></div>
-          </>
+          <div className={s.input}>
+            <Input />
+            <Input />
+            <input className={s.address} type="text" />
+          </div>
+          <div className={s.details}></div>
         </div>
       </Container>
+      <BasketFooter />
     </div>
   );
 }

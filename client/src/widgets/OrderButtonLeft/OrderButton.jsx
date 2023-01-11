79c3084981ece04bbfefb4c380bae2da7c5b9f96
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-11 12:19:26 +0300

    prod

---
+++ client/src/widgets/OrderButtonLeft/OrderButton.jsx
@@ -0,0 +1,12 @@
+import s from './OrderButton.module.css'
+import prev from "./Vector 1.svg";
+export function OrderButton({ text }) { 
+  return (
+    <>
+      <button className={s.oreder_button}>
+        <img className={ s.prev } src={prev} alt="" />
+        <p className={ s.btn_title }>{ text }</p>
+      </button>
+    </>
+  );
+}

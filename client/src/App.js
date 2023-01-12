547435f598ccfa283defed5bb8794689d8d90c39
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-12 17:52:40 +0300

    delete

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import { Main } from './UI/Main/Main'
 import { Basket } from './pages/Basket/Basket';
 import { Order } from './pages/Order/Order';
 import { OrderStatus } from './pages/OrderStatus/OrderStatus';
-
-import { BasketFooter } from './widgets/BasketFooter/BasketFooter'
 import { ModalWindowOne } from './pages/ ModalWindowOne/ ModalWindowOne';
 
 function App() {

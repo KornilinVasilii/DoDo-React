3a0b1790748131ff49e55465bc96948f33c2e82b
Vasilii Kornilin <v-kornilin@mail.ru>
2023-01-17 12:06:02 +0300

    add map card

--- server/app.js
+++ server/app.js
@@ -1,6 +1,16 @@
 import express from "express";
+import cors from "cors";
+import data from "./data.json" assert { type: "json" };
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use('/photos', express.static(`./assets/images/`));
+
+app.get("/api", (req, res) => {
+  return res.status(201).json(data);
+});
 
 app.listen(5000, () => { 
   console.log('Server has been started...')

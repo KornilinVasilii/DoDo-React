a824276da87316e4e5030549e07eade4078be000
Daniel <eltsoff75@gmail.com>
2023-01-18 09:49:00 +0300

    add

--- client/src/components/Popular/Popular.jsx
+++ client/src/components/Popular/Popular.jsx
@@ -2,14 +2,29 @@ import { Gridcards } from '../Gridcards/Gridcards';
 import { H2 } from '../H2/H2';
 import { SmallCards } from '../SmallCards/SmallCards';
 import s from './Popular.module.css'
-export function Popular({ data }) { 
+import { useState, useEffect } from 'react';
+
+export function Popular() { 
+    // const [popular, setPopular] = useState([]);
+
+    // useEffect(() => {
+    //   fetch('http://localhost:5000/combo', {
+    //     method: 'GET',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Access-Control-Allow-Origin': '*',
+    //     },
+    //   })
+    //     .then((popular) => popular.json())
+    //     .then((d) => setPopular(d.popular));
+    // }, []);
   return (
     <div className={s.popular}>
       <H2 text="Новое и популярное" />
       <Gridcards>
-            {data?.map((el, i) => (
+            {/* {popular.map((el, i) => (
         <SmallCards key={i} {...el} />
-      ))}
+      ))} */}
       </Gridcards>
     </div>
   );
